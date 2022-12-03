e6868279a756df609bbadcd8449d5ab574ff2da4
freddie-entity <nguyentrungtinc4nh1516@gmail.com>
2022-12-03 20:20:10 +0700

    Add MetalLB and Dashboard addons

---
+++ resources/toolchain/kubernetes-dashboard/main.tf
@@ -0,0 +1,30 @@
+resource "helm_release" "kubernetes_dashboard" {
+  name       = "kubernetes-dashboard"
+  repository = "https://kubernetes.github.io/dashboard/"
+  chart      = "kubernetes-dashboard"
+  version    = "0.13.7"
+  wait = false
+  timeout   = "1200"
+
+  namespace        = var.namespace
+
+#   values = [file("kubernetes-dashboard-values.yaml")]
+}
+
+resource "null_resource" "wait_for_kubernetes_dashboard" {
+  triggers = {
+    key = uuid()
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+      printf "\nWaiting for the Kubernetes dashboard...\n"
+      kubectl wait --namespace ${var.namespace} \
+        --for=condition=ready pod \
+        --selector=app.kubernetes.io/component=controller \
+        --timeout=90s
+    EOF
+  }
+
+  depends_on = [helm_release.kubernetes_dashboard]
+}
